78ba6c7f7cf2e222a36e8cf6d65309bff6febeb2
Bayashat <baiashat2002@gmail.com>
2024-01-30 16:41:58 +0600

    changed date format to dd.mm.yyyy

--- app/repository.py
+++ app/repository.py
@@ -1,12 +1,12 @@
 class CommentsRepository:
     def __init__(self):
         self.comments = [
-            {"id": 1, "name": "Ali", "comment_date": "2021-01-01", "context": "This is a comment", "category": "Positive"},
-            {"id": 2, "name": "Ahmed", "comment_date": "2024-01-30", "context": "I hate the World", "category": "Negative"},
-            {"id": 3, "name": "Mohamed", "comment_date": "2023-12-31", "context": "Happy New Year everyone", "category": "Positive"},
-            {"id": 4, "name": "Sayed", "comment_date": "2022-01-01", "context": "I love the World", "category": "Positive"},
-            {"id": 5, "name": "Mahmoud", "comment_date": "2021-01-01", "context": "This is a comment", "category": "Positive"},
-            {"id": 6, "name": "Mahmoud", "comment_date": "2021-01-01", "context": "This is a comment", "category": "Positive"},
+            {"id": 1, "name": "Ali", "comment_date": "15.02.2022", "context": "This is a comment", "category": "Positive"},
+            {"id": 2, "name": "Ahmed", "comment_date": "30.01.2023", "context": "I hate the World", "category": "Negative"},
+            {"id": 3, "name": "Mohamed", "comment_date": "31.12.2024", "context": "Happy New Year everyone", "category": "Positive"},
+            {"id": 4, "name": "Sayed", "comment_date": "27.09.2020", "context": "I love the World", "category": "Positive"},
+            {"id": 5, "name": "Mahmoud", "comment_date": "04.03.2002", "context": "I want to kill everyone", "category": "Negative"},
+            {"id": 6, "name": "Mahmoud", "comment_date": "19.05.2015", "context": "Hope the world be better and better", "category": "Positive"},
         ]
 
     def get_all(self):
