9ee6899104bf4f01535d53e12cb73be90b91d8fa
Bayashat <baiashat2002@gmail.com>
2024-01-30 16:27:41 +0600

    Basic logics for showing comment

---
+++ app/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse, Response
+from fastapi.templating import Jinja2Templates
+
+from .repository import CommentsRepository
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+repository = CommentsRepository()
+
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/comments")
+def get_cars(request: Request, category: str = "Positive"):
+    print(category)
+    comments = repository.get_all()
+    filtered_comments = [comment for comment in comments if comment["category"] == category]
+    return templates.TemplateResponse(
+        "comments/index.html",
+        {"request": request, "comments": filtered_comments},
+    )
+
+@app.get("/cars/new")
+def new_car(request: Request, response: Response):
+    return templates.TemplateResponse("cars/new.html", {"request": request})
+    
+@app.post("/cars/new")
+def post_car(request: Request, car_name:str = Form(...), year:str = Form(...)):
+    repository.save(
+        {
+            "name": car_name,
+            "year": year
+        }
+    )
+    return RedirectResponse("/cars", status_code=303)
+    
